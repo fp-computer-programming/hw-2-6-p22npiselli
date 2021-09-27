d2579ac03f935e1089c6df0dd9afa8cf5e7a8bfc
p22npiselli <p22npiselli@fairfieldprep.org>
2021-09-27 10:20:28 -0400

    hw 2-6

---
+++ hw_2-6-2.py
@@ -0,0 +1,13 @@
+# Author: Nolan (AMDG) 9/23/2021
+
+text = inpit("please enter radius")
+radius = int(text)
+
+text = input("please enter height")
+height = input(text)
+
+volume = (3.14 * 9 radius ** 2) * height
+print(volume)
+
+sa = (2 * 3.14 * (radius ** 2)) + (2 * 3.14 * radius)
+print(sa)
